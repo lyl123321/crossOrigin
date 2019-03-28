89dfeccad116386ae7b35f3bf2244c45db6aa9dc
lyl123321 <2099367442@qq.com>
2019-03-28 12:03:09 +0800

    post

--- resource/crossTest.js
+++ resource/crossTest.js
@@ -1,5 +1,9 @@
-//jsonp
-const jsonp = function({url, data, jsonp}) {
+//简单的跨域请求jsonp即可，复杂的cors，窗口之间JS跨域postMessage，开发环境下接口跨域用nginx反向代理或node中间件比较方便。
+//1、跨域发起接口请求
+//2、跨域获取 DOM 或 JS 对象
+//3、跨域获取 Cookie、LocalStorage 和 IndexDB
+//1.1、jsonp 跨域发起 get 请求
+const jsonp = function({url, data}) {
 	return new Promise((resolve, reject) => {
 		// 处理传参成xx=yy&aa=bb的形式
 		const handleData = function(data) {
@@ -8,15 +12,14 @@ const jsonp = function({url, data, jsonp}) {
 			
 			return keys.reduce((acc, cur, index) => {
 				const value = data[cur];
-				const flag = index !== keysLen - 1 ? '&' : ''
+				const flag = index !== keysLen - 1 ? '&' : '';
 				return `${acc}${cur}=${value}${flag}`;
 			}, '');
 		};
 		
 		// 动态创建script标签
 		const script = document.createElement('script');
-		jsonp = jsonp || 'jsonpCb';
-		script.src = `${url}?${handleData(data)}&callback=${jsonp}`;
+		script.src = `${url}?${handleData(data)}&callback=jsonpCb`;
 		document.body.appendChild(script);
 		document.body.removeChild(script);
 		
@@ -36,3 +39,40 @@ jsonp({
 	}
 })
 .then(res => console.log(res));
+
+//1.2、空 iframe 加 form 跨域发起 post 请求
+const iframePost = function({url, data}) {
+	const iframe = document.createElement('iframe');
+	iframe.name = 'iframePost';
+	iframe.style.display = 'none';
+	document.body.appendChild(iframe);
+	iframe.onload = function() {
+		console.log('success post');
+		console.log(iframe.contentWindow.document.body.textContent);
+	}
+	
+	const form = document.createElement('form');
+	const node = document.createElement('input');
+	form.action = url;
+	form.method = 'post';
+	form.target = iframe.name;
+	form.style.display = 'none';
+	
+	for(let key of Object.keys(data)) {
+		node.name = key;
+		node.value = data[key];
+		form.appendChild(node.cloneNode());
+	}
+	
+	document.body.appendChild(form);
+	form.submit();
+	document.body.removeChild(form);
+}
+
+iframePost({
+	url: 'http://localhost:8081/iframePost',
+	data: {
+		id: 'lyl',
+		psw: '123456'
+	}
+});
